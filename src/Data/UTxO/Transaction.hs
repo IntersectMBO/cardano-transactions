deef2ffce55773f8621c2cd5cf44e0860f3b5151
KtorZ <matthias.benkort@gmail.com>
2020-04-07 18:36:59 +0200

    Add golden tests and proper haddock documentation

--- src/Data/UTxO/Transaction.hs
+++ src/Data/UTxO/Transaction.hs
@@ -3,8 +3,16 @@
 {-# LANGUAGE TypeFamilyDependencies #-}
 
 module Data.UTxO.Transaction
-    ( UTxOTransaction (..)
-    , N(..)
+    (
+    -- * Overview
+    -- $overview
+
+    -- * Abstraction
+      MkPayment (..)
+    , ErrMkPayment (..)
+
+    -- * Note about type-level constraints
+    -- $note
     ) where
 
 import Prelude
@@ -16,52 +24,154 @@ import Data.Word
 import Numeric.Natural
     ( Natural )
 
--- | Construct a UTxO Transaction from inputs and outputs. The interface is
--- pretty generic and may be a bit _frightening_ but it's in essence very simple
--- and ensure that the transaction is correct-by-construction.
+-- $overview
 --
--- For instance, constructing a transaction with one input and two inputs is as
--- simple as:
+-- This module offers an abstraction over the construction of simple UTxO
+-- transactions. Such a transaction is called a /Payment/. Payments are
+-- constructed by selecting inputs and outputs, and signing each input
+-- independently.
+--
+-- This can be represented as a state machine, such as:
 --
 -- @
--- let transaction = empty
---          & addInput   inp0
---          & addOutput  out0
---          & addOutput  out1
---          & lock
---          & addWitness wit0
---          & serialize
+--                          empty
+--                            |
+--      *-----------------*   |   *----------------*
+--      |                 |   |   |                |
+--      |                 v   v   v                |
+--      *--- addOutput ---=========--- addInput ---*
+--                            |
+--                            |
+--                          lock  *----------------*
+--                            |   |                |
+--                            |   v                |
+--                        =========--- signWith ---*
+--                            |
+--                            |
+--                        serialize
+--                            |
+--                            |
+--                            v
+--                           ...
 -- @
 --
--- The nice thing about this interface is that it lets client choose underlying
--- concrete types which remove a lot of the complexity AND, it prevents the
--- serialization of ill-formed transactions with missing inputs.
-class UTxOTransaction dom where
+-- Functions from the 'MkPayment' interfaces are designed to be easy to chain
+-- like pipes. To give a concrete example, let's see how to construct a
+-- transaction with /one input/ and /two outputs/:
+--
+-- @
+-- import Data.Function
+--     ( (&) )
+-- import qualified Data.UTxO.Transaction as Tx
+--
+-- bytes = Tx.empty
+--       & Tx.addInput  input0
+--       & Tx.addOutput output0
+--       & Tx.addOutput output1
+--       & Tx.lock
+--       & Tx.signWith  key0
+--       & Tx.serialize
+-- @
+--
+-- The reason why the 'lock' step is required is because, in order to create
+-- witnesses, one must produce signatures of the whole transaction body (inputs
+-- + outputs). Hence, signatures can only be added once all inputs and all
+-- outputs have already been addded.
+--
+-- This is correct-by-construction thanks to the 'lock' step and the distinction
+-- between 'CoinSel' and 'Tx'.
+
+-- | Abstract interface for constructing a /Payment/.
+class MkPayment dom where
+    -- | Some initial extra data needed to construct and or serialize the
+    -- transaction. This is a free-type which is implementation-specific.
+    --
+    -- For example, Byron transactions requires the underlying protocol magic in
+    -- order to produce witnesses. This is an appropriate place for.
     type Init    dom :: *
 
+    -- | Type representation of transaction 'Input'
     type Input   dom = inp | inp -> dom
+
+    -- | Type representation of transaction 'Output'
     type Output  dom = out | out -> dom
+
+    -- | Type representation of transaction 'SignKey'
     type SignKey dom = key | key -> dom
 
-    type CoinSel dom = (sel :: N -> N -> *) | sel -> dom
-    type Tx      dom = (tx  :: N -> *)      | tx  -> dom
+    -- | An intermediate type for representing a transaction under construction.
+    -- 'Input' and 'Output' can still be added to a 'CoinSel' and must be added
+    -- before trying to sign any of the inputs.
+    type CoinSel dom = sel | sel -> dom
 
-    -- * Primitive types construction
-    mkInput   :: ByteString -> Word32  -> Maybe (Input dom)
-    mkOutput  :: ByteString -> Natural -> Maybe (Output dom)
-    mkSignKey :: ByteString -> Maybe (SignKey dom)
+    -- | A transaction is a locked 'CoinSel' to which it is no longer possible
+    -- to add new 'Input' and or 'Output'. Only 'Tx' can be serialized.
+    type Tx      dom = tx  | tx  -> dom
 
     -- * Transaction construction
-    empty     :: Init dom   -> CoinSel dom Zero Zero
-    addInput  :: Input dom  -> CoinSel dom n m -> CoinSel dom (Some n) m
-    addOutput :: Output dom -> CoinSel dom n m -> CoinSel dom n (Some m)
+    empty     :: Init dom -> CoinSel dom
+    addInput  :: Input  dom -> CoinSel dom -> CoinSel dom
+    addOutput :: Output dom -> CoinSel dom -> CoinSel dom
 
     -- * Transaction signature
-    lock       :: CoinSel dom (Some n) (Some m) -> Tx dom (Some n)
-    addWitness :: SignKey dom -> Tx dom (Some n) -> Tx dom n
+    lock      :: CoinSel dom -> Tx dom
+    signWith  :: SignKey dom -> Tx dom -> Tx dom
 
     -- * Transaction serialization
-    serialize :: Tx dom Zero -> ByteString
+    serialize :: Tx dom -> Either ErrMkPayment ByteString
 
--- | An inductive type for counting inputs  & signatures.
-data N = Zero | Some N
+-- | Possible errors occuring when constructing a /Payment/. This can in
+-- practice be safely ignored if the transaction is known to be well-formed.
+data ErrMkPayment
+    = MissingInput
+        -- ^ Payments must have at least one input.
+    | MissingOutput
+        -- ^ Payments must have at least one output
+    | MissingSignature
+        -- ^ Payments must have a signature for each input.
+    deriving (Show, Eq)
+
+-- $note
+--
+-- It is relatively easy to tweak the interface below to get it fully type-safe
+-- by using an inductive type, such as:
+--
+-- @
+-- data N = Zero | Some N
+--
+-- class MkPayment dom where
+--     type Init    dom :: *
+--
+--     type Input   dom = inp | inp -> dom
+--     type Output  dom = out | out -> dom
+--     type SignKey dom = key | key -> dom
+--
+--     type CoinSel dom = (sel :: N -> N -> *) | sel -> dom
+--     type Tx      dom = (tx  :: N -> *)      | tx  -> dom
+--
+--     empty     :: Init dom -> CoinSel dom Zero Zero
+--
+--     addInput  :: Input  dom -> CoinSel dom n m -> CoinSel dom (Some n) m
+--     addOutput :: Output dom -> CoinSel dom n m -> CoinSel dom n (Some m)
+--
+--     lock      :: CoinSel dom (Some n) (Some m) -> Tx dom (Some n)
+--
+--     signWith :: SignKey dom -> Tx dom (Some n) -> Tx dom n
+--
+--     serialize :: Tx dom Zero -> ByteString
+-- @
+--
+-- Such approach is great for ensuring that the transaction can't be ill-formed
+-- and works well when manually constructing the transaction as shown above.
+--
+-- It gets however quite complex when one wants to start folding or mapping on a
+-- list of inputs or outputs where their numbers isn't known at compile-time!
+-- Indeed, each call to 'addInput' does actually return a new type!
+--
+-- Let's say we want to write the following function:
+--
+-- @
+-- addMany :: [Input dom] -> CoinSel dom n m -> CoinSel dom ? m
+-- @
+--
+-- What should be the type of `?`. This can't possibly known at compile-time.
--- src/Data/UTxO/Transaction/Cardano/Byron.hs
+++ src/Data/UTxO/Transaction/Cardano/Byron.hs
@@ -1,37 +1,19 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module Data.UTxO.Transaction.Cardano.Byron
     (
-    -- * Type
-    Byron
+    -- * Constructing Primitives
+      mkInput
+    , mkOutput
+    , mkSignKey
 
-    -- * Helpers
-    , mkOutput'
-    , decodeAddressBase58
-    , encodeAddressBase58
+    -- * Converting From Bases
+    , fromBase16
+    , fromBase58
     ) where
-
-import Codec.CBOR.Read
-    ( deserialiseFromBytes )
-import Data.ByteString
-    ( ByteString )
-import Data.List.NonEmpty
-    ( NonEmpty )
-import Data.UTxO.Transaction
-    ( N (..), UTxOTransaction (..) )
-import Data.Word
-    ( Word32 )
-import GHC.Exts
-    ( IsList (fromList) )
-import Numeric.Natural
-    ( Natural )
-
-import qualified Codec.CBOR.Write as CBOR
-import qualified Data.ByteString.Lazy as BL
-import qualified Data.List.NonEmpty as NE
-import qualified Data.Text.Encoding as T
 
 import Cardano.Binary
     ( FromCBOR (..), ToCBOR (..) )
@@ -51,123 +33,175 @@ import Cardano.Chain.UTxO
     , mkTxAux
     )
 import Cardano.Crypto.Hashing
-    ( decodeHash, hash )
+    ( AbstractHash (..), hash )
 import Cardano.Crypto.ProtocolMagic
     ( ProtocolMagicId (..) )
 import Cardano.Crypto.Signing
     ( SignTag (..), Signature, SigningKey (..), VerificationKey (..) )
 import Cardano.Crypto.Wallet
     ( toXPub, xprv )
+import Cardano.Crypto.Wallet.Encrypted
+    ( EncryptedKey, encryptedCreateDirectWithTweak, unEncryptedKey )
+import Codec.CBOR.Read
+    ( deserialiseFromBytes )
+import Crypto.Error
+    ( CryptoFailable (..) )
+import Crypto.Hash
+    ( Blake2b_256, digestFromByteString )
+import Data.ByteArray.Encoding
+    ( Base (..), convertFromBase, convertToBase )
+import Data.ByteString
+    ( ByteString )
+import Data.ByteString.Base58
+    ( bitcoinAlphabet, decodeBase58 )
+import Data.List.NonEmpty
+    ( NonEmpty )
+import Data.Text
+    ( Text )
+import Data.UTxO.Transaction
+    ( ErrMkPayment (..), MkPayment (..) )
+import Data.Word
+    ( Word32 )
+import GHC.Exts
+    ( IsList (fromList) )
+import Numeric.Natural
+    ( Natural )
 
 import qualified Cardano.Chain.UTxO as CC
 import qualified Cardano.Crypto.Signing as CC
+import qualified Codec.CBOR.Write as CBOR
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.List.NonEmpty as NE
+import qualified Data.Text.Encoding as T
 
+-- | Construct a payment 'Input' for /Byron/ from primitive types.
+--
+-- __example__:
+--
+-- >>> mkInput 14 =<< fromBase16 "3b402651...aad1c0b7"
+-- Just (Input ...)
+mkInput
+    :: Word32
+        -- ^ Input index.
+    -> ByteString
+        -- ^ Input transaction id.
+    -> Maybe (Input Byron)
+mkInput ix bytes =
+    case digestFromByteString @Blake2b_256 bytes of
+        Just txId -> Just $ TxInUtxo (AbstractHash txId) ix
+        Nothing -> Nothing
+
+-- | Construct a payment 'Output' for /Byron/ from primitive types.
+--
+-- __example__:
+--
+-- >>> mkOutput 42 =<< fromBase58 "Ae2tdPwU...DnXy319f"
+-- Just (Output ...)
+mkOutput
+    :: Natural
+        -- ^ Output value, in Lovelace (1 Ada = 1e6 Lovelace).
+    -> ByteString
+        -- ^ Output Address
+    -> Maybe (Output Byron)
+mkOutput n bytes =
+    case (fromCBOR' bytes, mkLovelace (fromIntegral n)) of
+        (Right addr, Right coin) -> Just $ TxOut addr coin
+        _ -> Nothing
+  where
+    fromCBOR' = fmap snd . deserialiseFromBytes fromCBOR .  BL.fromStrict
+
+-- | Construct a 'SignKey' for /Byron/ from primitive types.
+--
+-- __example__:
+--
+-- >>> mkSignKey =<< fromBase16 "3b402651...aad1c0b7"
+-- Just (SignKey ...)
+mkSignKey
+    :: ByteString
+        -- ^ A extended address private key and its chain code.
+        -- The key __must be 96 bytes__ long, internally made of two concatenated parts:
+        --
+        -- @
+        -- BYTES = PRV | CC
+        -- PRV   = 64OCTET  # a 64 bytes Ed25519 extended private key
+        -- CC    = 32OCTET  # a 32 bytes chain code
+        -- @
+    -> Maybe (SignKey Byron)
+mkSignKey bytes
+    | BS.length bytes /= 96 = Nothing
+    | otherwise = do
+        let ekey = encryptedCreateDirectWithTweak bytes (mempty :: ByteString)
+        case xprv (unEncryptedKey ekey) of
+            Right prv -> Just (SigningKey prv)
+            Left{} -> Nothing
+
+--
+-- ByteString Decoding
+--
+
+-- | Convert a base16 encoded 'Text' into a raw 'ByteString'
+fromBase16 :: Text -> Maybe ByteString
+fromBase16 = either (const Nothing) Just . convertFromBase Base16 . T.encodeUtf8
+
+-- | Convert a base58 encoded 'Text' into a raw 'ByteString'
+fromBase58 :: Text -> Maybe ByteString
+fromBase58 = decodeBase58 bitcoinAlphabet . T.encodeUtf8
+
+--
+-- MkPayment instance
+--
 
 data Byron
 
--- | Internal type use for constructing a coin selection
-data ByronCoinSel (nInp :: N) (nOut :: N) = ByronCoinSel
-    { csProtocolMagicId :: ProtocolMagicId
-    , csInputs :: [TxIn]
-    , csOutputs :: [TxOut]
-    }
-
--- | Internal type use for constructing a transaction
-data ByronTx (nWit :: N) = ByronTx
-    { txProtocolMagicId :: ProtocolMagicId
-    , txInputs :: NonEmpty TxIn
-    , txOutputs :: NonEmpty TxOut
-    , txSigData :: TxSigData
-    , txWits :: [TxInWitness]
-    }
-
-instance UTxOTransaction Byron where
+instance MkPayment Byron where
     type Init Byron = ProtocolMagicId
 
     type Input   Byron = TxIn
     type Output  Byron = TxOut
     type SignKey Byron = SigningKey
 
-    type CoinSel Byron = ByronCoinSel
-    type Tx      Byron = ByronTx
+    type CoinSel Byron =
+        (ProtocolMagicId, [TxIn], [TxOut])
 
-    mkInput :: ByteString -> Word32 -> Maybe TxIn
-    mkInput bytes ix =
-        case decodeHash (T.decodeUtf8 bytes) of
-            Right txId -> Just $ TxInUtxo txId ix
-            Left{} -> Nothing
+    type Tx Byron = Either
+        ErrMkPayment
+        (ProtocolMagicId, NonEmpty TxIn, NonEmpty TxOut, TxSigData, [TxInWitness])
 
-    mkOutput :: ByteString -> Natural -> Maybe TxOut
-    mkOutput bytes n =
-        case (fromCBOR' bytes, mkLovelace (fromIntegral n)) of
-            (Right addr, Right coin) -> Just $ TxOut addr coin
-            _ -> Nothing
+    empty :: ProtocolMagicId -> CoinSel Byron
+    empty pm = (pm, mempty, mempty)
+
+    addInput :: TxIn -> CoinSel Byron -> CoinSel Byron
+    addInput inp (pm, inps, outs) = (pm, inp : inps, outs)
+
+    addOutput :: TxOut -> CoinSel Byron -> CoinSel Byron
+    addOutput out (pm, inps, outs) = (pm, inps, out : outs)
+
+    lock :: CoinSel Byron -> Tx Byron
+    lock (_pm, [], _outs) = Left MissingInput
+    lock (_pm, _inps, []) = Left MissingOutput
+    lock (pm, inps, outs) =
+        Right (pm, neInps, neOuts, sigData, mempty)
       where
-        fromCBOR' = fmap snd . deserialiseFromBytes fromCBOR .  BL.fromStrict
+        sigData = TxSigData $ hash $ CC.UnsafeTx neInps neOuts (mkAttributes ())
+        neInps  = NE.fromList $ reverse inps
+        neOuts  = NE.fromList $ reverse outs
 
-    -- FIXME
-    -- 'xprv' assumes that the bytes represent the concatenation of
-    --
-    -- - An extended private key (32 bytes)
-    -- - An extended public key (64 bytes)
-    -- - A chain code (32 bytes)
-    --
-    -- whereas here, we probably really want the bytes to be only representing
-    --
-    -- - An extended private key (32 bytes)
-    -- - A chain code (32 bytes)
-    --
-    -- The extended public key can and should be computed from the extended
-    -- public key.
-    mkSignKey :: ByteString -> Maybe SigningKey
-    mkSignKey bytes = case xprv bytes of
-        Left{} -> Nothing
-        Right prv -> Just $ SigningKey prv
-
-    empty :: ProtocolMagicId -> CoinSel Byron Zero Zero
-    empty pm = ByronCoinSel pm mempty mempty
-
-    addInput :: TxIn -> CoinSel Byron n m -> CoinSel Byron (Some n) m
-    addInput inp cs = cs { csInputs = inp : csInputs cs }
-
-    addOutput :: TxOut -> CoinSel Byron n m -> CoinSel Byron n (Some m)
-    addOutput out cs = cs { csOutputs = out : csOutputs cs }
-
-    lock :: CoinSel Byron (Some n) (Some m) -> Tx Byron (Some n)
-    lock cs = ByronTx
-        { txProtocolMagicId = csProtocolMagicId cs
-        , txInputs  = neInps
-        , txOutputs = neOuts
-        , txSigData = TxSigData $ hash $ CC.UnsafeTx neInps neOuts (mkAttributes ())
-        , txWits = mempty
-        }
-      where
-        -- Safe because inputs are guaranteed to be non-empty by construction
-        neInps = NE.fromList $ reverse $ csInputs cs
-        -- Safe because ouputs are guaranteed to be non-empty by construction
-        neOuts = NE.fromList $ reverse $ csOutputs cs
-
-    addWitness :: SigningKey -> Tx Byron (Some n) -> Tx Byron n
-    addWitness (SigningKey prv) tx =
-        tx { txWits = VKWitness vk sig : txWits tx }
+    signWith :: SigningKey -> Tx Byron -> Tx Byron
+    signWith _ (Left e) = Left e
+    signWith (SigningKey prv) (Right (pm, inps, outs, sigData, wits)) =
+        Right (pm, inps, outs, sigData, VKWitness vk sig : wits)
       where
         vk :: VerificationKey
         vk = VerificationKey (toXPub prv)
 
-        pm :: ProtocolMagicId
-        pm = txProtocolMagicId tx
-
         sig :: Signature TxSigData
-        sig = CC.sign pm SignTx (SigningKey prv) (txSigData tx)
+        sig = CC.sign pm SignTx (SigningKey prv) sigData
 
-    serialize :: Tx Byron Zero -> ByteString
-    serialize tx = CBOR.toStrictByteString $ toCBOR $ mkTxAux
-        (CC.UnsafeTx (txInputs tx) (txOutputs tx) (mkAttributes ()))
-        (fromList $ reverse $ txWits tx)
-
--- | An alternative to 'mkOutput' which works directly from 'Address'
-mkOutput' :: Address -> Natural -> Maybe TxOut
-mkOutput' addr n =
-    case mkLovelace (fromIntegral n) of
-        Left{} -> Nothing
-        Right coin -> Just $ TxOut addr coin
+    serialize :: Tx Byron -> Either ErrMkPayment ByteString
+    serialize (Left e) = Left e
+    serialize (Right (pm, inps, outs, sigData, wits))
+        | NE.length inps /= length wits = Left MissingSignature
+        | otherwise = Right $ CBOR.toStrictByteString $ toCBOR $ mkTxAux
+            (CC.UnsafeTx inps outs (mkAttributes ()))
+            (fromList $ reverse wits)
