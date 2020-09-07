bb4486b57fa1f78899047791e1455fd22d29a13f
Pawel Jakubas <pawel.jakubas@iohk.io>
2020-09-07 19:38:18 +0200

    add gloden tests for shelley witnesses

--- test/Data/UTxO/Transaction/Cardano/ShelleySpec.hs
+++ test/Data/UTxO/Transaction/Cardano/ShelleySpec.hs
@@ -22,18 +22,11 @@ import Data.Text
 import Data.UTxO.Transaction
     ( ErrMkPayment (..), MkPayment (..) )
 import Data.UTxO.Transaction.Cardano.Helpers
-    ( fromBase16, fromBase58, fromBech32 )
+    ( fromBase16, fromBech32 )
 import Data.UTxO.Transaction.Cardano.Shelley
-    ( NetworkId (..)
-    , Shelley
-    , mkByronSignKey
-    , mkInit
-    , mkInput
-    , mkOutput
-    , mkShelleySignKey
-    )
+    ( NetworkId (..), Shelley, mkInit, mkInput, mkOutput, mkShelleySignKey )
 import Data.Word
-    ( Word32, Word64 )
+    ( Word32, Word64, Word8 )
 import Numeric.Natural
     ( Natural )
 import Test.Hspec
@@ -42,6 +35,8 @@ import Test.Hspec.QuickCheck
     ( prop )
 import Test.QuickCheck
     ( Arbitrary (..)
+    , conjoin
+    , counterexample
     , forAll
     , oneof
     , quickCheck
@@ -50,6 +45,8 @@ import Test.QuickCheck
     , (===)
     )
 
+import qualified Codec.CBOR.Encoding as CBOR
+import qualified Codec.CBOR.Pretty as CBOR
 import qualified Data.ByteString as BS
 import qualified Data.UTxO.Transaction as Tx
 
@@ -81,106 +78,86 @@ spec = do
 
         it "Missing Input" $ do
             let result = Tx.empty dummyInit
-                       & Tx.addOutput (unsafeMkOutput 14 (addrs !! 0))
+                       & Tx.addOutput (unsafeMkOutput 14 addrTxt)
                        & Tx.lock
                        & Tx.serialize
             result `shouldBe` Left MissingInput
 
         it "Missing Output" $ do
             let result = Tx.empty dummyInit
-                       & Tx.addInput (unsafeMkInput 0 (txids !! 0))
+                       & Tx.addInput (unsafeMkInput 0 txidTxt)
                        & Tx.lock
                        & Tx.serialize
             result `shouldBe` Left MissingOutput
 
         it "Missing Signature" $ do
             let result = Tx.empty dummyInit
-                       & Tx.addInput (unsafeMkInput 0 (txids !! 0))
-                       & Tx.addOutput (unsafeMkOutput 1400000 (addrs !! 0))
+                       & Tx.addInput (unsafeMkInput 0 txidTxt)
+                       & Tx.addOutput (unsafeMkOutput 1400000 addrTxt)
                        & Tx.lock
                        & Tx.serialize
             result `shouldBe` Left MissingSignature
 
-    describe "(integration tests) Transaction Construction" $ do
-        it "1 input, 1 output (DSL) - Shelley witnesses" $ do
-            compareIntegrationTesting integrationTesting__1_1 $ Tx.empty dummyInit
-                & Tx.addInput (unsafeMkInput 0 (txids !! 0))
-                & Tx.addOutput (unsafeMkOutput (10000000 - 2000000 - 129700) (addrs !! 1))
-                & Tx.addOutput (unsafeMkOutput 2000000 (addrs !! 2))
-                & Tx.lock
-                & Tx.signWith (unsafeMkShelleySignKey (keys !! 0))
-                & Tx.serialize
-        it "1 input, 1 output (DSL) - Byron witnesses" $ do
-            compareIntegrationTesting integrationTesting__1_1_byron $ Tx.empty dummyInit'
-                & Tx.addInput (unsafeMkInput 0 (txids !! 0))
-                & Tx.addOutput (unsafeMkOutput' (10000000 - 1000000 - 138200) (byronAddrs !! 0))
-                & Tx.addOutput (unsafeMkOutput 1000000 (addrs !! 2))
-                & Tx.lock
-                & Tx.signWith (uncurry unsafeMkByronSignKey (byronKeys !! 0))
-                & Tx.serialize
-        it "1 input, 1 output (DSL) - Shelley witnesses" $ do
-            let addr2 = BS.pack $ 1 : replicate 64 1
-            let addr3 = BS.pack $ 1 : replicate 64 2
-            let txId = BS.pack $ replicate 32 0
+    describe "Golden tests" $ do
+        it "1 input, 2 outputs (DSL) - Shelley witnesses" $ do
             let amtInp = 10000000
             let amtFee = 129700
             let amtOut = 2000000
             let amtChange = amtInp - amtOut - amtFee
-            let wit = BS.pack $ replicate 96 0
-
-            compareIntegrationTesting integrationTesting $ Tx.empty (mkInit Mainnet 7750 (fromIntegral amtFee))
-                & Tx.addInput (unsafeMkInput1 0 txId)
-                & Tx.addOutput (unsafeMkOutput1 amtOut addr2)
-                & Tx.addOutput (unsafeMkOutput1 amtChange addr3)
+            compareGolden integrationTesting__1_2_shelley $
+                Tx.empty (mkInit Mainnet 7750 (fromIntegral amtFee))
+                & Tx.addInput (unsafeMkInputFromBS 0 txid)
+                & Tx.addOutput (unsafeMkOutputFromBS amtOut (addr 1))
+                & Tx.addOutput (unsafeMkOutputFromBS amtChange (addr 2))
                 & Tx.lock
-                & Tx.signWith (unsafeMkShelleySignKey1 wit)
+                & Tx.signWith (unsafeMkShelleySignKeyFromBS (wit 0))
+                & Tx.serialize
+
+        it "2 inputs, 3 outputs (DSL) - Shelley witnesses" $ do
+            let amtInp1 = 10000000
+            let amtInp2 = 10000000
+            let amtFee = 135200
+            let amtOut1 = 6000000
+            let amtOut2 = 6000000
+            let amtChange = amtInp1 + amtInp2 - amtOut1 - amtOut2 - amtFee
+            compareGolden integrationTesting__2_3_shelley $
+                Tx.empty (mkInit Mainnet 7750 (fromIntegral amtFee))
+                & Tx.addInput (unsafeMkInputFromBS 0 txid)
+                & Tx.addInput (unsafeMkInputFromBS 1 txid)
+                & Tx.addOutput (unsafeMkOutputFromBS amtOut1 (addr 2))
+                & Tx.addOutput (unsafeMkOutputFromBS amtOut2 (addr 3))
+                & Tx.addOutput (unsafeMkOutputFromBS amtChange (addr 4))
+                & Tx.lock
+                & Tx.signWith (unsafeMkShelleySignKeyFromBS (wit 0))
+                & Tx.signWith (unsafeMkShelleySignKeyFromBS (wit 1))
                 & Tx.serialize
 
 --
--- Input for integration testing - probably temporary
+-- Golden
 --
 
-compareIntegrationTesting
+compareGolden
     :: Show e
     => ByteString
         -- ^ An expected encoded result
     -> Either e ByteString
         -- ^ The actual string received
     -> IO ()
-compareIntegrationTesting _want (Left e)   = expectationFailure (show e)
-compareIntegrationTesting want (Right got) = quickCheck $ withMaxSuccess 1 $
-    want === got
+compareGolden _want (Left e)   = expectationFailure (show e)
+compareGolden want (Right got) = quickCheck $ withMaxSuccess 1 $
+    -- NOTE Using QuickCheck here simply for getting better counter examples
+    -- than HSpec in case of failure...
+    conjoin (uncurry (===) <$> zip (lines prettyWant) (lines prettyGot))
+        & counterexample ("Got:  " <> prettyGot)
+        & counterexample ("Want: " <> prettyWant)
+  where
+    prettyWant =
+        CBOR.prettyHexEnc $ CBOR.encodePreEncoded want
+    prettyGot  =
+        CBOR.prettyHexEnc $ CBOR.encodePreEncoded got
 
-integrationTesting__1_1 :: ByteString
-integrationTesting__1_1 = unsafeB16
-    "83a4008182582046ba24e81def97dd70971bd0e2bd93176def634a5140280a1\
-    \667df57c0b6fe5700018282583901c5dd8de847cd8f3613bb5c90bc282dfb8a\
-    \3c582b868bd11b71b744f8d3e37167a44ccd635d108b145c1730a4bfccbf49d\
-    \cc788b7194c4d881a0078175c825839011f4595045be843e5eca0cc3be5757a\
-    \ed6c9fe77d97b64c13ea50005e40d8e350a139670d5dc3e0ad7ac00646d96ee\
-    \513640709a1708f98761a001e8480021a0001faa403191e46a10081825820c5\
-    \b31478ced8c20ea3349f4833d12db2068483a96e3d77d5ccd942046cbeeca25\
-    \840087987af97bdab10b9eb0ba5dd659f9eb58b8515c3ae8f63c799d9369f09\
-    \2bd739449e4c63f867dcbd91f8e4c7dbdb0b70519c3fee0b9349cb6b066acff\
-    \8c501f6"
-
-integrationTesting__1_1_byron :: ByteString
-integrationTesting__1_1_byron = unsafeB16
-    "83a4008182582046ba24e81def97dd70971bd0e2bd93176def634a5140280a1\
-    \667df57c0b6fe5700018282584c82d818584283581cb8cab01f3117517f219d\
-    \fe927c1339c718445422df246ac05b353e72a101581e581c2af08478866f967\
-    \927478899deb36c2f9b6713acd719672dc7b9e595001ab8705ac91a00873868\
-    \825839011f4595045be843e5eca0cc3be5757aed6c9fe77d97b64c13ea50005\
-    \e40d8e350a139670d5dc3e0ad7ac00646d96ee513640709a1708f98761a000f\
-    \4240021a00021bd803191e46a10281845820d2a1098573423204e6c3ac10a76\
-    \02f9b01aa32a5b65b8c75503450fc3b3ea317584022287a7079066cb5f4bb29\
-    \a6ec4f2f9f58a67cb0cc788cf58f78f75b2a179fd4c048bf472705ccaa10953\
-    \fa8b2c615470933543f02df6d521c4d98443747290a582006c552df3d160132\
-    \01a44025f79b7eae1cd697b3fb173f31110b97935c6ba6005822a101581e581\
-    \c2af08478866f967927478999c5a7e904b22c68abaf73cabeb0e6c400f6"
-
-integrationTesting :: ByteString
-integrationTesting = unsafeB16
+integrationTesting__1_2_shelley :: ByteString
+integrationTesting__1_2_shelley = unsafeB16
     "83a400818258200000000000000000000000000000000000000000000000000000\
     \000000000000000182825839010101010101010101010101010101010101010101\
     \010101010101010101010101010101010101010101010101010101010101010101\
@@ -191,31 +168,36 @@ integrationTesting = unsafeB16
     \11c1445c79590526990bfa73cbb3732b54ef322daa142e6884023410f8be3c16e9\
     \bd52076f2bb36bf38dfe034a9f04658e9f56197ab80ff6"
 
+integrationTesting__2_3_shelley :: ByteString
+integrationTesting__2_3_shelley = unsafeB16
+    "83a400828258200000000000000000000000000000000000000000000000000000\
+    \000000000000008258200000000000000000000000000000000000000000000000\
+    \000000000000000000010183825839010202020202020202020202020202020202\
+    \020202020202020202020202020202020202020202020202020202020202020202\
+    \0202020202021a005b8d8082583901030303030303030303030303030303030303\
+    \030303030303030303030303030303030303030303030303030303030303030303\
+    \03030303031a005b8d808258390104040404040404040404040404040404040404\
+    \040404040404040404040404040404040404040404040404040404040404040404\
+    \040404041a007801e0021a0002102003191e46a10082825820130ae82201d7072e\
+    \6fbfc0a1884fb54636554d14945b799125cf7ce38d477f5158405835ff78c6fc5e\
+    \4466a179ca659fa85c99b8a3fba083f3f3f42ba360d479c64ef169914b52ade49b\
+    \19a7208fd63a6e67a19c406b4826608fdc5307025506c307825820010000000000\
+    \00000000000000000000000000000000000000000000000000005840e8e769ecd0\
+    \f3c538f0a5a574a1c881775f086d6f4c845b81be9b78955728bffa7efa54297c6a\
+    \5d73337bd6280205b1759c13f79d4c93f29871fc51b78aeba80ef6"
+
 --
 -- Internal
 --
 dummyInit :: Init Shelley
 dummyInit = mkInit Mainnet 7750 129700
 
-dummyInit' :: Init Shelley
-dummyInit' = mkInit Mainnet 7750 138200
 
 unsafeMkInput :: Word32 -> Text -> Input Shelley
 unsafeMkInput ix str = fromJust $ mkInput ix (unsafeB16 str)
 
 unsafeMkOutput :: Natural -> Text -> Output Shelley
 unsafeMkOutput n str = fromJust $ mkOutput n (unsafeBech32 str)
-
-unsafeMkOutput' :: Natural -> Text -> Output Shelley
-unsafeMkOutput' n str = fromJust $ mkOutput n (unsafeBase58 str)
-
-unsafeMkShelleySignKey :: Text -> SignKey Shelley
-unsafeMkShelleySignKey str = fromJust $ mkShelleySignKey (unsafeB16 str)
-
-unsafeMkByronSignKey :: Text -> Text -> SignKey Shelley
-unsafeMkByronSignKey str1 str2 =
-    let addr = unsafeBase58 str1
-    in fromJust $ mkByronSignKey addr (unsafeB16 str2)
 
 unsafeB16 :: Text -> ByteString
 unsafeB16 = fromMaybe (error msg) . fromBase16
@@ -225,56 +207,32 @@ unsafeBech32 :: Text -> ByteString
 unsafeBech32 = fromMaybe (error msg) . fromBech32
   where msg = "unable to decode bech32 string."
 
-unsafeBase58 :: Text -> ByteString
-unsafeBase58 = fromMaybe (error msg) . fromBase58
-  where msg = "unable to decode base58 string."
+unsafeMkOutputFromBS :: Natural -> ByteString -> Output Shelley
+unsafeMkOutputFromBS n bytes = fromJust $ mkOutput n bytes
 
-unsafeMkOutput1 :: Natural -> ByteString -> Output Shelley
-unsafeMkOutput1 n bytes = fromJust $ mkOutput n bytes
+unsafeMkShelleySignKeyFromBS :: ByteString -> SignKey Shelley
+unsafeMkShelleySignKeyFromBS bytes = fromJust $ mkShelleySignKey bytes
 
-unsafeMkShelleySignKey1 :: ByteString -> SignKey Shelley
-unsafeMkShelleySignKey1 bytes = fromJust $ mkShelleySignKey bytes
-
-unsafeMkInput1 :: Word32 -> ByteString -> Input Shelley
-unsafeMkInput1 ix bytes = fromJust $ mkInput ix bytes
+unsafeMkInputFromBS :: Word32 -> ByteString -> Input Shelley
+unsafeMkInputFromBS ix bytes = fromJust $ mkInput ix bytes
 
 --
 -- Test Vectors
 --
 
-txids :: [Text]
-txids =
-    -- the input !! 0 was created due to this tx
-    [ "46ba24e81def97dd70971bd0e2bd93176def634a5140280a1667df57c0b6fe57"
-    ]
+txidTxt :: Text
+txidTxt = "46ba24e81def97dd70971bd0e2bd93176def634a5140280a1667df57c0b6fe57"
 
-addrs :: [Text]
-addrs =
-      -- input of txid !! 0 with ix = 0
-    [ "addr1q9wteuqmnkywcz9zefjyp0wn3tctz9xgxm8fpcmyzn9uypknudck0fzve4346yytz3wpwv9yhlxt7jwuc7ytwx2vfkyq75lt2a"
-      -- change
-    , "addr1q8zamr0gglxc7dsnhdwfp0pg9hac50zc9wrgh5gmwxm5f7xnudck0fzve4346yytz3wpwv9yhlxt7jwuc7ytwx2vfkyquaaj4r"
-     -- output
-    , "addr1qy05t9gyt05y8e0v5rxrhet40tkke8l80ktmvnqnafgqqhjqmr34pgfevux4mslq44avqpjxm9hw2ymyquy6zuy0npmq030jj2"
-    ]
+addrTxt :: Text
+addrTxt =
+    "addr1q9wteuqmnkywcz9zefjyp0wn3tctz9xgxm8fpcmyzn9uypknudck0fzve4346yytz3wpw\
+    \v9yhlxt7jwuc7ytwx2vfkyq75lt2a"
 
-byronAddrs :: [Text]
-byronAddrs =
-    [ "DdzFFzCqrht3xS8ySv9t4MVM1euWvcC6WbzPZKkr6WqE7zcW8nSS9A3eGvcrMKvANGF6auayauFQjazrQtC7T8hx9CvXKq4U3qS2ApAC"
-    ]
-keys :: [Text]
-keys =
-    -- witness for input of txid !! 0 with ix = 0
-    [ "400cd8a0c260e5aafe1768bf077815351fedf1bb698e1fb4a32903d52f006541\
-      \c9dfe5b6c62f5c30aa958711233696d2d3f8486547a9e2f928d064f5752acfa8\
-      \1d028f8516adad09a8efe3a0209e7e75d6bc61265f48d7471acdd2bf7ee7b23d"
-    ]
+addr :: Word8 -> ByteString
+addr b = BS.pack $ 1 : replicate 64 b
 
-byronKeys :: [(Text,Text)]
-byronKeys =
-    [ ( "DdzFFzCqrhsfRTAFKYtEMjB1vy5fTth3QEitVbMBuk5r9Um6Uf2bWYj8cSYfbad9MLKokM2Y5FhybrJqCgUDVPNPkgG6oa33VLQ6jugc"
-      , "c323bec83ccf2e39ee42d499acd5ee7ade10e822270b4479f8e98c8c8e8abf0842dc7d\
-        \e2c0e69e52982743c73a75ea06ef87c80b94d380c83a9a99060488438f06c552df3d16\
-        \013201a44025f79b7eae1cd697b3fb173f31110b97935c6ba600"
-      )
-    ]
+txid :: ByteString
+txid = BS.pack $ replicate 32 0
+
+wit :: Word8 -> ByteString
+wit b = BS.pack $ replicate 96 b
